93a626a7e6c776ce53c6ad4a21eb68f7bdcefc00
Hitarth Sharma <hitarth.sharma@outlook.com>
2017-04-09 17:08:08 +1200

    Attempt to fix the live chat code

--- html.js
+++ html.js
@@ -36,20 +36,17 @@ module.exports = React.createClass({
             __html: this.props.body
           }}/>
           <script src={prefixLink(`/bundle.js?t=${BUILD_TIME}`)}/>
-          <!--Start of Tawk.to Script-->
-          <script type="text/javascript">
-            var Tawk_API = Tawk_API || {},
-              Tawk_LoadStart = new Date();
-            (function() {
-              var s1 = document.createElement("script"),
-                s0 = document.getElementsByTagName("script")[0];
-              s1.async = true;
-              s1.src = 'https://embed.tawk.to/58e9bdb3f7bbaa72709c532a/default';
-              s1.charset = 'UTF-8';
-              s1.setAttribute('crossorigin', '*');
-              s0.parentNode.insertBefore(s1, s0);
-            })();
-          </script>
-          <!--End of Tawk.to Script-->
+
+          <script dangerouslySetInnerHTML={{ __html: `var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+          (function() {
+            var s1 = document.createElement("script"),
+              s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/58e9bdb3f7bbaa72709c532a/default';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+          })();` />
         </body>
       </html>
